020e68306f46c4c250b3f11418912a8a50b89a5c
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-01-30 23:35:23 +0000

    an empty class that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
     pass
-
-
-my_square = Square
-print(type(my_square))
-print(my_square.__dict__)
