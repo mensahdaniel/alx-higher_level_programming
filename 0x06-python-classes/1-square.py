54445a28289a08c2c9fc5a22062896896200affc
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-01-30 23:56:06 +0000

    i created a module that has a private attribute called size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+this module defines an a class for representing square of private size atributes
+"""
+class Square:
+    def __init__(self,size):
+        """
+        initialises a sqaure with a size
+        """
+        self.__size = size
+
