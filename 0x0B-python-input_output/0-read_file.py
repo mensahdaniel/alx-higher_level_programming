96ecefc1a9ec9cbaab1e8623b1d5710cc76ee9d3
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-02-08 00:51:32 +0000

    a function that reads a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("filename",encoding=utf-8) as fi:
-        print(fi.read(),end="")
+    with open("filename", encoding=utf-8) as fi:
+        print(fi.read(), end="")
