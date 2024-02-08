3683d8d0104b2d94b6bba89f1250f10d05c6e2ec
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-02-08 00:57:31 +0000

    a function that reads a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" defines a text file reading options"""
+
+
 def read_file(filename=""):
+    """ opens and prints the content of the file in utf-8 to s.o"""
     with open("filename", encoding=utf-8) as fi:
         print(fi.read(), end="")
