432961bf76971994bbf38bac01e410ddcaa257a9
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-02-13 14:27:46 +0000

    a function that returnss objects of the directory

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" a fuction that returns objects of that fuction"""
+
+def lookup(obj):
+    """ returns a list of obj attributes."""
+    return dir((obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this are task that i did on python inheritace in alx
