1b3862d69cda9fa6b2047253ee93c79d0a4a27a8
mensahdaniel <mensahdaniel5858910@gmail.com>
2024-05-23 05:00:20 +0100

    completed

--- 0x0E-SQL_more_queries/1-creat_usr.sql
+++ 0x0E-SQL_more_queries/1-creat_usr.sql
@@ -1,4 +1,5 @@
--- just doing somthing 
+-- creates the database
+-- the usr in local host 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
